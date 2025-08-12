5359a21dd5774fdb43bad2e9ac96af8285f0359e
Mwikali <mwikalisharon749@gmail.com>
2025-08-12 12:07:19 +0300

    Add files via upload

---
+++ python_wk2_assignment.py
@@ -0,0 +1,26 @@
+#empty list
+My_list = []
+
+#Append the following elements to my_list: 10, 20, 30, 40.
+My_list.append(10)
+My_list.append(20)    
+My_list.append(30)
+My_list.append(40)
+
+#Insert the value 15 at the second position in the list.
+My_list.insert(1, 15)
+
+#Extend my_list with another list: [50, 60, 70].
+My_list.extend([50, 60, 70])
+
+#Remove the last element from my_list.
+My_list.pop(-1)
+
+#Sort my_list in ascending order.
+My_list.sort()
+
+#Find and print the index of the value 30 in my_list.
+index_of_30 = My_list.index(30)
+print(f"The index of 30 in my_list is: {index_of_30}")
+
+print(My_list)
